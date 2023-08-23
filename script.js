6da074973c4ade7fd806c3cd9c2f12dcf41224a1
Sovzl <soovze@gmail.com>
2023-08-23 14:42:15 +0200

    Remove resetOpacity function, edit colorpicker function

--- script.js
+++ script.js
@@ -1,87 +1,77 @@
-const resetButton = document.getElementById('resetButton')
-const slider = document.querySelector('.slider')
-const gridSizeLabel = document.getElementById('num')
-const gridArea = document.getElementById('gridArea')
-const rainbowButton = document.getElementById('rainbow')
-const darkenButton = document.getElementById('darken')
-const colorPickerButton = document.getElementById('colorPicker')
-const sizeInfo = document.getElementById('num')
-const gridOnOff = document.getElementById('gridOnOff')
+const resetButton = document.getElementById('resetButton');
+const slider = document.querySelector('.slider');
+const gridSizeLabel = document.getElementById('num');
+const gridArea = document.getElementById('gridArea');
+const rainbowButton = document.getElementById('rainbow');
+const darkenButton = document.getElementById('darken');
+const colorPickerButton = document.getElementById('colorPicker');
+const sizeInfo = document.getElementById('num');
+const gridOnOff = document.getElementById('gridOnOff');
 
- let currentMode = colorPicker;
+let currentMode = colorPicker;
 
- gridOnOff.addEventListener('click', () => {
+gridOnOff.addEventListener('click', () => {
   const boxes = document.querySelectorAll('.box');
   boxes.forEach((box) => {
     box.classList.toggle('with-border');
   });
 });
 
- slider.addEventListener('input', () => {
+slider.addEventListener('input', () => {
   const gridSize = parseInt(slider.value);
   gridSizeLabel.textContent = gridSize;
   gridArea.style.setProperty('--columns', gridSize); // Update the --columns custom property
   createGrid(gridSize);
 });
 
-
-
-
 function createGrid(gridSize) {
   gridArea.innerHTML = '';
   sizeInfo.textContent = gridSize;
-  for (let i = 0; i < gridSize * gridSize; i++){
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const box = document.createElement('div');
-    box.classList.add('box')
-    gridArea.appendChild(box)
+    box.classList.add('box');
+    gridArea.appendChild(box);
     box.addEventListener('mouseenter', () => {
-      currentMode(box)
-    })
+      currentMode(box);
+    });
+  }
+}
+
+colorPickerButton.addEventListener('change', () => {
+  currentMode = colorPicker;
+});
+
+
+function colorPicker(box) {
+  box.style.backgroundColor = colorPickerButton.value;
+  const currentOpacity = parseFloat(box.style.opacity) || 0;
+  if (currentOpacity > 0.1) {
+    box.style.opacity = '';
   }
 }
 
 
-
-colorPickerButton.addEventListener('change', () => {
-  currentMode = colorPicker;
-  resetOpacity();
-
-});
-
-function colorPicker(box) {
-  box.style.backgroundColor = colorPickerButton.value;
-  resetOpacity();
-}
-
-resetButton.addEventListener('click', resetGrid)
+resetButton.addEventListener('click', resetGrid);
 
 function resetGrid() {
- const boxes = document.querySelectorAll('.box')
- boxes.forEach((box) => {
-  box.style.backgroundColor = '';
- })
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach((box) => {
+    box.style.backgroundColor = '';
+  });
 }
 
 rainbowButton.addEventListener('click', () => {
   currentMode = rainbow;
-})
+});
 
 function rainbow(box) {
   box.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 70%)`;
-
 }
 
 darkenButton.addEventListener('click', () => {
   currentMode = darken;
 });
 
-
-// Add event listener to darken button
-darkenButton.addEventListener('click', () => {
-  currentMode = darken;
-});
-
-// Function to darken the box color
 function darken(box) {
   const currentOpacity = parseFloat(box.style.opacity) || 0;
   const newOpacity = currentOpacity + 0.1;
@@ -94,14 +84,6 @@ function darken(box) {
     const newColor = `rgba(0, 0, 0, ${newOpacity})`;
     box.style.backgroundColor = newColor;
   }
-}
-
-
-function resetOpacity() {
-  const boxes = document.querySelectorAll('.box');
-  boxes.forEach((box) => {
-    box.style.opacity = ''; // Reset the opacity to its default value
-  });
 }
 
 
